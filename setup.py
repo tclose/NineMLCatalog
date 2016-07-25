0c8a5ea5f6aa7a8dc3d7689495583f96999a99ac
Tom Close <tclose@oist.jp>
2016-07-26 09:39:28 +1000

    Getting setup.py to work

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python
 
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
+src_dir = os.path.abspath(os.path.dirname(__file__))
+catalog_dir = os.path.join(src_dir, 'ninemlcatalog', 'catalog')
 
 try:
-    os.symlink('../xml', 'catalog')
+    # Create a symlink to the catalog path inside the package directory so it
+    # gets installed along side the package
+    os.symlink(os.path.join(src_dir, 'xml'), catalog_dir)
     setup(
         name="ninemlcatalog",
         version="1.0",
-        packages=['ninemlcatalog'],
+        packages=find_packages(),
         package_data={'ninemlcatalog': ['catalog']},
         author=("Thomas G. Close"),
         author_email="nineml-users@incf.org",
         description=(
             "A collection of 9ML models and basic Python functions for "
             "accessing them"),
-        long_description=open("../README.rst").read(),
+        long_description=open("README.rst").read(),
         license="MIT Licence",
         keywords="computational neuroscience modeling interoperability XML",
         url="http://nineml.incf.org",
@@ -28,8 +32,8 @@ try:
                      'Operating System :: OS Independent',
                      'Programming Language :: Python :: 2',
                      'Topic :: Scientific/Engineering'],
-        install_requires=['nineml'],
+        install_requires=[], # ['nineml'],
         tests_require=['nose']
     )
 finally:
-    os.unlink('catalog')
+    os.unlink(catalog_dir)
